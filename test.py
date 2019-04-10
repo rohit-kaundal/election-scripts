cd55ae7fc6c6d2a2d872f59113f90fa537fb8929
Rohit Kaundal <kaundal.rohit@gmail.com>
2019-04-10 16:53:53 +0530

    Added Google Drive API Cred

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 # Test file
 # author: Rohit Kaundal
 
-def init():
-    print("Done")
+import pandas
 
+
+def loadXl(filename):
+    xl = pandas.read_excel(filename, sheet_name="Advertisements")
+    return xl
 
 def main():
-    init()
+    adsFile = loadXl("ads.xlsx")
+    print(adsFile.keys())
 
 if __name__ == '__main__':
     main()
